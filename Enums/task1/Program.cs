5afb5bcad0ba1102b9c954198b208c31feae776e
Fazliddin <you@gofurov.com>
2026-07-16 09:28:50 -0400

    adding extra examples

--- Enums/task1/Program.cs
+++ Enums/task1/Program.cs
@@ -16,10 +16,95 @@ namespace _task1
 
     class Program
     {
+
+        enum TrafficLight
+        {
+            red,
+            yellow,
+            green
+        }
+        enum Season
+        {
+            Winter,
+            Spring,
+            Summer,
+            Autumn
+        }
         static void Main (string[] args)
         {
-            Console.WriteLine("Hafta Kunini Kiriting:");
 
+            Console.WriteLine("1-12 gacha bolgan oy raqamini kiriting");
+            if(int.TryParse(Console.ReadLine(), out int month))
+            {
+                if(month < 1 || month > 12)
+                {
+                    Console.WriteLine("Xato! Oy raqami faqat 1 dan 12 gacha bo'lishi mumkin.");
+                    return;
+                }
+
+                Season joriyFasl;
+
+                switch (month)
+                {
+                    case 12:
+                    case 1:
+                    case 2:
+                        joriyFasl =  Season.Winter;
+                        break;
+                    case 3:
+                    case 4:
+                    case 5:
+                        joriyFasl = Season.Spring;
+                        break;
+                    case 6:
+                    case 7:
+                    case 8:
+                        joriyFasl = Season.Summer;
+                        break;
+                    default: // 9, 10, 11 oylar uchun
+                        joriyFasl = Season.Autumn;
+                        break;
+                }
+                Console.WriteLine($"Hozirgi fasl: {joriyFasl}");
+            }
+            else
+            {
+                Console.WriteLine("Iltimos, faqat Son kiriting!");
+            }
+           
+
+            /*
+            Console.WriteLine("Enter a color:");
+            string color =  Console.ReadLine();
+
+            if(Enum.TryParse<TrafficLight>(color, out TrafficLight light))
+            {
+                switch (light)
+                {
+                    case TrafficLight.red:
+                        Console.WriteLine("Toxta");
+                        break;
+                    case TrafficLight.yellow:
+                        Console.WriteLine("Tayyorlan");
+                        break;
+                    case TrafficLight.green:
+                        Console.WriteLine("Yugur");
+                        break;
+                    default:
+                    Console.WriteLine("Bunday svetofor rangi yo'q");
+                    break;
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("Soz xato kirtildi");
+            }
+            */
+
+            
+            /*
+            Console.WriteLine("Hafta Kunini Kiriting:");
     
             if(int.TryParse(Console.ReadLine(), out int inputNumber))
             {
@@ -38,16 +123,13 @@ namespace _task1
             {
                 Console.WriteLine("Iltimos, Faqat Son Kriting!");
             }
-
-            
-
+            */
 
         }
 
          static bool IsWeekend(WeekDay day)
         {
             return day == WeekDay.Saturday || day == WeekDay.Sunday;
-           
         }
     } 
 
--- Enums/task1/obj/Debug/net10.0/task1.AssemblyInfo.cs
+++ Enums/task1/obj/Debug/net10.0/task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+623d8869e418f5834f6da61b45ffcfa3d294c2e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddd9b5c3c185755407eb8187271987c98892bdfd")]
 [assembly: System.Reflection.AssemblyProductAttribute("task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
