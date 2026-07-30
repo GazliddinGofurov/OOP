96fc0ed9470f6c44b359e7f807e65c5b95ebd553
Fazliddin <my@email.com>
2026-07-30 02:17:49 -0400

    task2

---
+++ Generic/task2/LastElement.cs
@@ -0,0 +1,7 @@
+class lastElement<T>
+{
+    static public T getLastItem (T[] array)
+    {
+        return array[array.Length -1];
+    }
+}
---
+++ Generic/task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _task3
+{
+    class Program
+    {
+        static void  Main (string [] args)
+        {
+            double[] buses = {12.34,23.001,93.12,45.01};
+            string[]restrauns = {"Alo", "Bahor", "Tanovar","Versal"};
+            int[]numbers = {1,3,5,9,2};
+
+
+            System.Console.WriteLine(lastElement<double>.getLastItem(buses));
+            Console.WriteLine(lastElement<string>.getLastItem(restrauns));
+            Console.WriteLine(lastElement<int>.getLastItem(numbers));
+            
+        }
+    }
+}
