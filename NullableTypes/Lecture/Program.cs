ea1734cd39172940ac6d59cc195c06bd7410ad7b
Fazliddin <my@email.com>
2026-08-27 06:22:53 -0400

    nullable

---
+++ NullableTypes/Lecture/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace _NullableLecture
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            // there are two ways to Define a nullable int value type 
+            // int? optionalValue =  null;
+            Nullable<int> optionalValue;
+
+            // Code here sets the optionalValue
+            optionalValue =5;
+  
+            if (optionalValue.HasValue)
+            {
+                Console.WriteLine(optionalValue.Value);
+            }
+            else
+            {
+                Console.WriteLine("no value");
+            }
+
+            Console.WriteLine("\n");
+            //------------------------------------------------------
+            //Arithmetic operators with nullable types
+
+            int? a = 10;
+            int? b = null;
+            int?sum = a + b ; // sum =  null
+           
+           // int (a value type) must be declared with "?" to accept a null value
+           int? son = null;
+
+           // straing (a reference type) inherently accept a null value
+
+           string str =  null;
+
+        
+
+
+            
+        }
+
+    }
+   
+}
---
+++ NullableTypes/Lecture/Readme.txt
@@ -0,0 +1,10 @@
+C# Nullable Value Types
+In C#, nullable types represent the absence of a value and help prevent runtime errors by 
+allowing value types like int? or bool? to hold a null value.
+
+C# Nullable Operators
+In C#, operators for nullable value types include lifted operators,
+comparison and equality operators, and logical operators, particularly for the bool? type.
+
+C# Nullable Reference and Value Types
+In C#, reference types inherently support nullable values, while value types do not.
